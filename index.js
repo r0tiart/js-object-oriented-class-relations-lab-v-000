c158a7aef46c2d2dc2cee2c4d499e4212b73fb2f
r0tiart <antley.chu@gmail.com>
2017-09-29 13:25:01 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,61 @@
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
+let store = {drivers: [], trips: [], passengers: []}
+
+class Driver {
+  constructor(name, zipcode){
+    this.name = name
+    this.id = ++driverId
+    store.drivers.push(this)
+  }
+  trips(){
+    return store.trips.filter((trip)=> {
+      return trip.driverId === this.id
+    })
+  }
+  passengers(){
+    return this.trips().map((trip)=> {
+      return trip.passenger()
+    })
+  }
+}
+
+
+class Passenger {
+  constructor(name){
+    this.name = name
+    this.id = ++passengerId
+    store.passengers.push(this)
+  }
+
+  trips(){
+    return store.trips.filter((trip)=> {
+      return trip.passengerId === this.id
+    })
+  }
+  drivers(){
+    return this.trips().map((trip)=> {
+      return trip.driver()
+    })
+  }
+}
+
+
+
+
+class Trip {
+  constructor(driver, passenger, startingZip, endingZip){
+    this.driverId = driverId;
+    this.passengerId = passengerId;
+    this.id = ++tripId
+    store.trips.push(this)
+  }
+  driver(){
+    return store.drivers.find((driver) => { return driver.id === this.driverId })
+  }
+  passenger(){
+    return store.passengers.find((passenger) => { return passenger.id === this.passengerId })
+  };
+};
+
